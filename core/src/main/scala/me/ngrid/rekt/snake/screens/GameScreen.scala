e4cf7f5e1e1483d8ddb533f611096e253e9e3bb4
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2016-09-02 02:55:31 +0100

    A very sneaky snake

--- core/src/main/scala/me/ngrid/rekt/snake/screens/GameScreen.scala
+++ core/src/main/scala/me/ngrid/rekt/snake/screens/GameScreen.scala
@@ -3,6 +3,8 @@ package me.ngrid.rekt.snake.screens
 import com.badlogic.gdx.{Graphics, Input, ScreenAdapter}
 import com.badlogic.gdx.graphics.{Color, Texture}
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable
 
 /**
   *
@@ -11,18 +13,18 @@ object GameScreen extends ScreenAdapter {
 
   import com.badlogic.gdx.Gdx._
   import com.badlogic.gdx.graphics.GL20
+  val MOVE_TIME = 0.5F
 
   var batch: SpriteBatch = _
-  var snakeHead: Texture = _
-  val MOVE_TIME = 1F
 
   var timer = MOVE_TIME
+  var snake = Snake(0, 0)
 
-  var snake = Snake(direction = Left)
+  var apple: Apple = _
+  var appleShown = false
 
   override def show(): Unit = {
     batch = new SpriteBatch()
-    snakeHead = new Texture(files.internal("snake/snakehead.png"))
   }
 
   override def render(delta: Float): Unit = {
@@ -30,19 +32,17 @@ object GameScreen extends ScreenAdapter {
     snake = snake.redirect(queryInputForDirection(snake.direction))
 
     timer = if (timer <= 0) {
-      snake = snake.move(graphics.getWidth -> graphics.getHeight)
+      snake = snake.move(graphics.getWidth -> graphics.getHeight, apple)
       MOVE_TIME
     }
     else timer - delta
 
-    val c = Color.BLACK
-    gl.glClearColor(c.r, c.g, c.b, c.a)
-    gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-
-    batch.begin()
-    batch.draw(snakeHead, snake.x, snake.y)
-    batch.end()
+    checkAppleSnakeCollision()
+    placeApple()
+    clearScreen()
+    draw()
   }
+
 
   def queryInputForDirection(default: Direction): Direction = {
     if(input.isKeyPressed(Input.Keys.LEFT)) Left
@@ -51,13 +51,57 @@ object GameScreen extends ScreenAdapter {
     else if(input.isKeyPressed(Input.Keys.DOWN)) Down
     else default
   }
+
+  def placeApple(): Unit = {
+    if(!appleShown) {
+      do {
+        val x = MathUtils.random(graphics.getWidth / Snake.SNAKE_MOVEMENT - 1) * Snake.SNAKE_MOVEMENT
+        val y = MathUtils.random(graphics.getHeight / Snake.SNAKE_MOVEMENT - 1) * Snake.SNAKE_MOVEMENT
+        apple = Apple(x, y)
+        appleShown = true
+      } while (apple.x == snake.x && apple.x == snake.y)
+    }
+  }
+
+  def clearScreen(): Unit = {
+    val c = Color.BLACK
+    gl.glClearColor(c.r, c.g, c.b, c.a)
+    gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+  }
+
+  def draw(): Unit = {
+    batch.begin()
+    snake.draw(batch)
+    apple.draw(batch)
+    batch.end()
+  }
+
+  def checkAppleSnakeCollision(): Unit = {
+    if(appleShown && apple.x == snake.x && apple.y == snake.y) appleShown = false
+  }
 }
 
-case class Snake(x: Int = 0, y: Int = 0, direction: Direction) {
+object Snake {
   val SNAKE_MOVEMENT = 32
-  def move(boundaries: (Int, Int)): Snake = {
-    val(x1, y1) = checkBounds(direction.move((x, y), SNAKE_MOVEMENT), boundaries)
-    copy(x = x1, y = y1)
+
+  def apply(x: Int, y: Int): Snake = Snake(x, y, Left, List(BodyPart.head(x, y)))
+}
+
+case class Snake(x: Int = 0, y: Int = 0, direction: Direction, parts: List[BodyPart]) {
+  import Snake._
+
+  def move(boundaries: (Int, Int), apple: Apple): Snake = {
+    val head = BodyPart.head(checkBounds(direction.move((x, y), SNAKE_MOVEMENT), boundaries))
+
+    val body =
+      if(head.x == apple.x && head.y == apple.y) BodyPart.body(x, y) +: parts.tail
+      else BodyPart.body(x, y) +: parts.tail.dropRight(1)
+
+    copy(
+      x = head.x,
+      y = head.y,
+      parts = head +: body
+    )
   }
 
   def checkBounds(p: (Int, Int), b: (Int, Int)): (Int, Int) = {
@@ -70,25 +114,49 @@ case class Snake(x: Int = 0, y: Int = 0, direction: Direction) {
   def redirect(d: Direction): Snake =
     if(d != direction) copy(direction = d)
     else this
+
+  def draw(b: SpriteBatch): Unit = parts.foreach(_.draw(b))
+}
+
+case class Apple(x: Int, y: Int){
+  import com.badlogic.gdx.Gdx.files
+  val tex: Texture = new Texture(files.internal("snake/apple.png"))
+
+  def draw(b: SpriteBatch): Unit = b.draw(tex, x, y)
+}
+
+object BodyPart {
+  import com.badlogic.gdx.Gdx.files
+
+  def head(pos: (Int, Int)):BodyPart = head(pos._1, pos._2)
+  def head(x: Int, y: Int):BodyPart = BodyPart(x, y, new Texture(files.internal("snake/snakehead.png")))
+
+  def body(x: Int, y: Int) = BodyPart(x, y, new Texture(files.internal("snake/snakebody.png")))
+}
+
+case class BodyPart(x: Int, y: Int, tex: Texture) {
+  def draw(batch: SpriteBatch): Unit = batch.draw(tex, x, y)
 }
 
 trait Direction {
-  def move(point: (Int, Int), distance: Int): (Int, Int)
+  def move (point: (Int, Int), distance: Int): (Int, Int)
 }
 
 case object Left extends Direction {
-  override def move(point: (Int, Int), distance: Int) =
+  override def move (point: (Int, Int), distance: Int) =
     (point._1 - Math.abs(distance), point._2)
 }
 
 case object Right extends Direction {
-  override def move(point: (Int, Int), distance: Int) =
+  override def move (point: (Int, Int), distance: Int) = {
     (point._1 + Math.abs(distance), point._2)
+  }
 }
 
 case object Up extends Direction {
-  override def move(point: (Int, Int), distance: Int) =
+  override def move (point: (Int, Int), distance: Int) = {
     (point._1, point._2 + Math.abs(distance))
+  }
 }
 
 case object Down extends Direction {
